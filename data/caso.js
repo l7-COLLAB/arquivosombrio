16fb4cbc16258e8acfaca1096872a9718e04f65b
Pandora <l7526080@gmail.com>
2026-09-01 15:29:21 -0300

    Fix image path for Dyatlov case

--- data/caso.js
+++ data/caso.js
@@ -29,7 +29,7 @@ const casosArquivo = [
 
         status: "NÃO SOLUCIONADO",
 
-        imagem: "imagens/mistério/dyatlov.jpg",
+        imagem: "imagens/misterio/dyatlov.jpg",
 
         resumo:
             "Nove caminhantes experientes morreram durante uma expedição nas montanhas dos Urais sob circunstâncias que durante décadas alimentaram inúmeras teorias.",
