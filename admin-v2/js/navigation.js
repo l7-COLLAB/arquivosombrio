9c3c4b6ff6872a7cde6d90f77e80c02135641d3f
Pandora <l7526080@gmail.com>
2026-09-22 20:07:49 -0300

    admin-v2: restaurar separacao administrativa por categorias

--- admin-v2/js/navigation.js
+++ admin-v2/js/navigation.js
@@ -1,1 +1,7 @@
-export const NAV=[{group:"GERAL",items:[["overview","Visão geral"]]},{group:"CONTEÚDO",items:[["dossies","Dossiês"],["garimpo","Garimpo Sombrio"],["pericias","Perícia"],["lendas","Lendas"],["creepypastas","Creepypastas"],["biblioteca","Biblioteca"]]}];export const LABELS=Object.fromEntries(NAV.flatMap(g=>g.items));
+export const NAV=[
+{group:"GERAL",items:[["overview","Visão geral"]]},
+{group:"CONTEÚDO E EDIÇÃO",items:[["dossies","Dossiês"],["garimpo","Garimpo Sombrio"],["pericias","Perícia"],["lendas","Lendas"],["creepypastas","Creepypastas"],["biblioteca","Biblioteca"]]},
+{group:"PUBLICAÇÃO",items:[["rascunhos","Rascunhos"],["revisao","Revisão editorial"],["agendados","Agendamentos"],["midia","Mídia"],["fontes-central","Fontes"]]},
+{group:"COMUNIDADE",items:[["comentarios","Comentários"],["forum","Fórum"],["denuncias","Denúncias"],["sugestoes","Sugestões"]]},
+{group:"ADMINISTRAÇÃO",items:[["usuarios","Usuários"],["solicitacoes","Solicitações"],["historico","Histórico administrativo"]]}
+];export const LABELS=Object.fromEntries(NAV.flatMap(g=>g.items));
