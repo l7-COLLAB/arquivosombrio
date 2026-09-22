84f9ce1a9745e5a51aac7e63d398a7d3579f0dfa
Pandora <l7526080@gmail.com>
2026-09-22 12:54:59 -0300

    admin-v2: criar base isolada da nova central administrativa

---
+++ admin-v2/js/config.js
@@ -0,0 +1,5 @@
+export const SUPABASE_URL="https://iuhotznurbyujzbyhizf.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY="sb_publishable_bpAZ5EhYLIuVoE4Q97s_-A_XQwwRxUj";
+export const ADMIN_STORAGE_KEY="arquivo-sombrio-admin-v2";
+export const ADMIN_ROLE="admin";
+export const ADMIN_V2={development:true,publicWritesEnabled:false};
